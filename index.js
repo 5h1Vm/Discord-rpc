c3f15bf409d449f0251f512d6524501ebdd1a784
Shivam Singh <83861668+5h1Vm@users.noreply.github.com>
2021-06-14 02:33:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ details : "Main-text",
 state: "sub-text",
 assets : {
 large_image : "image name from discord/dev",
-large_text : "tool tip kindof thing", // THIS WILL SHOW AS "Playing <Status>" from the outisde
-// small_image : "123",
-// samll_text : "Electrostatics" // THIS WILL SHOW AS "Playing <Status>" from the outisde
+large_text : "tool tip kind of thing", // THIS WILL SHOW AS "Playing <Status>" from the outisde
+// small_image : "small image like pause and play buttons type",
+// samll_text : "same tool tip kinda" // THIS WILL SHOW AS "Playing <Status>" from the outisde
 },
 buttons : [{label : "button 1" , url : "button 1 url"},
 {label : "name button 2",url : "button 2 url"}]
